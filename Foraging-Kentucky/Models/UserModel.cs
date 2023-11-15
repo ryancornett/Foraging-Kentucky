aca1051d8cd974568ae55be21fec2ea2735f4072
Ryan Cornett <ryandcornett@gmail.com>
2023-11-14 20:13:44 -0500

    Async all possible methods and integrate child components

--- Foraging-Kentucky/Models/UserModel.cs
+++ Foraging-Kentucky/Models/UserModel.cs
@@ -2,7 +2,7 @@
 
 namespace Foraging_Kentucky.Models
 {
-    public class NewUserModel
+    public class UserModel
     {
         public string? Name { get; set; }
         public string? Email { get; set; }
